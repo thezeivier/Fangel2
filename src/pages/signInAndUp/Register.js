e33a6b6c1d86454252c312e5514a6a82f9c599cb
thezeivier <zbeizaga@gmail.com>
2021-03-27 20:05:46 -0500

    Al fin validación de formulario

--- src/pages/signInAndUp/Register.js
+++ src/pages/signInAndUp/Register.js
@@ -13,7 +13,7 @@ const Register = () => {
   const { register, handleSubmit, watch, errors } = useForm();
   const onSubmit = data => console.log(data);
   console.log(errors);
-  console.log(watch("example"))
+  // console.log(watch("example"))
   
   return (
     <>
@@ -23,10 +23,77 @@ const Register = () => {
             <SubtitleStyled>Regístrate para empezar</SubtitleStyled>
             <TextStyled>A unirte o crear comunidades</TextStyled>
             <Form center onSubmit={handleSubmit(onSubmit)}>
-              <InputStyled type="text" placeholder="Usuario" name="Username" ref={register({required: true, maxLength: 80})} />
-              <InputStyled type="text" placeholder="Correo electrónico" name="Email" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
-              <InputStyled type="password" placeholder="Contraseña" name="Password" ref={register({required: true, minLength: 8, maxLength: 100})} />
-              <InputStyled special type="text" placeholder="Código de invitación" name="Code" ref={register({required: true, minLength: 8, maxLength: 100})} />
+              <InputStyled 
+                type="text"
+                placeholder="Usuario"
+                name="username"
+                ref={
+                  register({
+                    required: true,
+                    maxLength: {
+                      value: 30,
+                      message: "El usuario debe ser menor a 30 caracteres.",
+                    },
+                    pattern: {
+                      value: /^[a-zA-Z0-9_]{5,}[a-zA-Z]+[0-9]*$/,
+                      message: "Usuario no válido o demasiado corto"
+                    }
+                  })
+                } 
+              />
+              {/* Falta posicionar */}
+              {errors.username? errors.username.message: ""} 
+
+              <InputStyled 
+                type="text"
+                placeholder="Correo electrónico"
+                name="email"
+                ref={
+                  register({
+                    required: true,
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: "Correo electrónico inválido"
+                    }
+                  })
+                } 
+              />
+              {/* Falta posicionar */}
+              {errors.email? errors.email.message: ""} 
+
+              <InputStyled 
+                type="password"
+                placeholder="Contraseña"
+                name="password"
+                ref={
+                  register({
+                    required: true,
+                    pattern: {
+                      value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                      message: "Tu contraseña es insegura."
+                    }
+                  })
+                } 
+              />
+              {errors.password? errors.password.message: ""}
+
+              <InputStyled 
+                special
+                type="text" 
+                placeholder="Código de invitación" 
+                name="code" 
+                ref={
+                  register({
+                    required: true,
+                    pattern: {
+                      value: /^(admin)?[A-Za-z\d]{12}$/i,
+                      message: "Rectifica tu código de invitación."
+                    }
+                  })
+                } 
+              />
+              {errors.code? errors.code.message: ""}
+
               <Description>
                 Usa el codigo que te proporcionó la persona que te invito a su comunidad
               </Description>
