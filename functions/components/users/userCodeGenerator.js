315fe3ed844f39d5cffbdce684034395fda84ce9
thezeivier <zbeizaga@gmail.com>
2021-04-01 04:11:33 -0500

    function timer de comunidades

--- functions/components/users/userCodeGenerator.js
+++ functions/components/users/userCodeGenerator.js
@@ -1,14 +1,13 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-admin.initializeApp();
-const db = admin.firestore();
 exports.userCodeGenerator = functions.firestore.document("/users/{documentId}").onCreate((snap) =>{
   if (snap.data().type === "admin") {
+    const db = admin.firestore();
     const batch = db.batch();
     const uid = snap.data().uid;
     const model = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     let code = "";
-    while (code.length < 13) {
+    while (code.length < 12) {
       code = code.concat(model.charAt(Math.round(Math.random()*model.length)));
     }
     batch.set(
--- functions/index.js
+++ functions/index.js
@@ -1,2 +1,4 @@
+const admin = require("firebase-admin");
 exports.userCodeGenerator = require("./components/users/userCodeGenerator");
-
+exports.roomTimerController = require("./components/communities/roomTimerController");
+admin.initializeApp();
