acd4672e2233a084760d9b96bcd26fe3c8fd1d1f
thezeivier <zbeizaga@gmail.com>
2021-04-06 04:42:00 -0500

    Hay un error al redireccionar desde Email-sended a login.

--- src/pages/signInAndUp/EmailSended.js
+++ src/pages/signInAndUp/EmailSended.js
@@ -47,7 +47,7 @@ const EmailSended = () => {
           <TextStyled>Acabamos de enviar un correo eléctronico a:</TextStyled>
           <Email>{location.state.email}</Email>
           <TextStyled>{selectedText.informationText}</TextStyled>
-          <LinkOtherPage>
+          <LinkOtherPage onClick={() => window.location.reload()}>
             <Link to={"/login"}>Regresar al Inicio de sesión</Link>
           </LinkOtherPage>
         </ExternalsWrapper>
