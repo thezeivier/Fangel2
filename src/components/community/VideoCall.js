24b3d061c66b2fe264b4fd19923e6553a0617df7
thezeivier <zbeizaga@gmail.com>
2021-04-08 22:05:05 -0500

    new push

--- src/components/community/VideoCall.js
+++ src/components/community/VideoCall.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Jitsi from 'react-jitsi'
-import fangel from '../general/icons/iconFangel.svg'
 import spinner from '../spinner/MainSpinner'
 
 const interfaceConfig = {
@@ -10,24 +9,25 @@ const interfaceConfig = {
   CONNECTION_INDICATOR_DISABLED: true,
   DEFAULT_BACKGROUND: '#252525',
   DEFAULT_LOCAL_DISPLAY_NAME: '',
+  DISABLE_PRESENCE_STATUS: true,
   DISABLE_JOIN_LEAVE_NOTIFICATIONS: true,
   HIDE_INVITE_MORE_HEADER: true,
   MOBILE_APP_PROMO: false,
   SHOW_CHROME_EXTENSION_BANNER: false,
   TOOLBAR_BUTTONS: [
-    // "microphone",
-    // "camera",
-    // "fullscreen",
+    "microphone",
+    "camera",
+    "fullscreen",
     // "fodeviceselection",
     // "hangup",
     // "profile",
     // "chat",
     // "settings",
     // "videoquality",
-    // "tileview",
+    "tileview",
     // "download",
     // "help",
-    // "mute-everyone"
+    "mute-everyone"
     // 'security'
   ]
 };
@@ -36,15 +36,8 @@ const config = {
   defaultLanguage: "es",
   prejoinPageEnabled: false,
   enableWelcomePage: false,
-    // The hex value for the colour used as background
-    backgroundColor: '#0ASDFG',
-    // The url for the image used as background
-    // backgroundImageUrl: 'https://example.com/background-img.png',
-    // The anchor url used when clicking the logo image
-    logoClickUrl: 'https://hola.com',
-    // The url used for the image used as logo
-    logoImageUrl: fangel,
-
+  disableProfile: true,
+  enableInsecureRoomNameWarning: false,
 };
 
 const VideoCall = ({dataUser}) => {
