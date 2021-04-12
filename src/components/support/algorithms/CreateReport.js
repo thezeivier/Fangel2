ddeda5f099864d531f07a9f78c02b05dfe625e1d
thezeivier <zbeizaga@gmail.com>
2021-04-11 20:44:32 -0500

    Reporte funcional.

---
+++ src/components/support/algorithms/CreateReport.js
@@ -0,0 +1,49 @@
+export const CreateReport = async (data, firestore, imageRecovered, storage, userFromDB) => {
+    const batch = firestore.batch()
+    const uid = userFromDB.uid
+    const type = userFromDB.type
+    const reportRef = firestore.collection("reports").doc(uid)
+    if(data.reportDescription){
+        reportImageSender(storage, uid, imageRecovered, type)
+        batch.set(
+            reportRef,
+            {
+                email: userFromDB.email,
+                message: data.reportDescription,
+                uid,
+                user: userFromDB.username,
+                type: userFromDB.type,
+            },
+            {merge: true}
+        )
+    
+        return await batch.commit()
+        .then(() =>{
+            return true
+        }).catch(()=>{
+            return false
+        })
+    }else{
+        return false
+    }
+}
+
+const reportImageSender = async (storage, uid, reportImage, type) => {
+    let storageRef = storage.ref()
+    let nameOfImage = uid.concat(new Date().getTime())
+    let imageRenamed = new File([reportImage], `${nameOfImage}.jpeg`, {type: 'image/jpeg'})
+    let initialType = reportImage.type.substr(0, 5)
+    if(reportImage && (initialType === "image" )){
+        let routePath = `reports/${type}/${imageRenamed.name}`
+        let uploadTask  = storageRef.child(routePath).put(imageRenamed)
+        uploadTask.on('state_changed', (result)=>{
+        let progress = (result.bytesTransferred/result.totalBytes) * 100
+        // console.log('Subido' + progress + '%')
+        })
+        return await uploadTask.then(async ()=>{
+        // console.log("upload success")
+        })
+    }else{
+        return false
+    }
+}
