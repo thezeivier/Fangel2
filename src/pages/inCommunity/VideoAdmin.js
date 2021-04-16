3ee1e5a26a1275fdbf122bb21c349f8ea360307a
Drakembite <drakembite@gmail.com>
2021-04-16 00:01:42 -0500

    AlerWarning para comunidad sin acciones, agregado

--- src/pages/inCommunity/VideoAdmin.js
+++ src/pages/inCommunity/VideoAdmin.js
@@ -1,10 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import MainVideoUser from './../../components/community/MainVideoUser'
 import RegHeader from './../../components/general/RegHeader'
 import VideoHeader from './../../components/general/VideoHeader'
 import Footer from './../../components/general/Footer'
+import { AlertContainer } from './../../components/community/styles/sMainVideo'
 import { ContainerForCommunity } from './../../components/general/InternalLayout'
 import MainSettingsAdmin from './../../components/settingsForAdmin/MainSettingsAdmin'
+
+import { ReactComponent as WarningSVG } from './../../components/community/icons/warning.svg'
+import { ReactComponent as CloseSVG } from './../../components/general/icons/close.svg'
 
 import MainSpinner from '../../components/spinner/MainSpinner'
 
@@ -19,15 +23,25 @@ const VideoAdmin = ({communityData, isAdmin}) => {
     setModalIsOpen(false)
   }
 
-  console.log(modalIsOpen)
-
   return (
     <>
+      {
+        modalIsOpen ? 
+        <AlertContainer>
+          <WarningSVG onClick={closeModal} />
+          <div className="alertDescriptionContainer">
+            <p>Quedan 5 minutos para que se cierre tu comunidad</p>
+            <a>Extender una hora más</a>
+          </div>
+          <CloseSVG className="closeAlertWarning" />
+        </AlertContainer>
+        : <></>
+      }
       {
         !communityData?
         <MainSpinner/>:
         <ContainerForCommunity> {/* Sin margenes en moviles y tablets ni footer ni regHeader,
-          wrapper sin paddign en moviles y tablet*/}
+        wrapper sin paddign en moviles y tablet*/}
           <RegHeader /> {/* Solo para moviles */}
           <VideoHeader communityData={communityData} isSettings="none" closeModal={closeModal} modalIsOpen={modalIsOpen} />
           <MainVideoUser communityData={communityData} open={openMoldal} isAdmin={isAdmin}/>
