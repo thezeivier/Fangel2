2dcf5bb638590c978692f6e405567c5b416b6583
thezeivier <zbeizaga@gmail.com>
2021-04-16 19:00:52 -0500

    Redireccionamiento a agradecimiento.

--- src/components/support/MainReport.js
+++ src/components/support/MainReport.js
@@ -3,7 +3,7 @@ import {AppContext} from '../../App'
 import Wrapper from './../general/Wrapper'
 import ReturnPage from './../general/ReturnPage'
 import {useFirestore, useStorage} from 'reactfire'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import { useForm } from 'react-hook-form';
 import { TitleStyled, TextStyled, SubtitleStyled, TextAreaStyled } from './../../themes/internalRecyclableStyles'
 import { ButtonStyled, ButtonsContainer, OnlyDesktop } from './styles/sMainSupport'
@@ -14,7 +14,7 @@ const MainReport = () => {
   const storage = useStorage()
   const contextFromApp = useContext(AppContext)
   const [imageRecovered, setImageRecovered] = useState()
-  const [reportCreated, setReportCreated] = useState()
+  const [reportCreated, setReportCreated] = useState(false)
   const { register, handleSubmit, errors } = useForm()
   const onSubmit = async data => {
     const result = await CreateReport(data, firestore, imageRecovered, storage, contextFromApp.userFromDB)
@@ -33,30 +33,36 @@ const MainReport = () => {
   
   return (
     <main>
-      <Wrapper>
-        <OnlyDesktop>
-          <TitleStyled bottom>Reporta un problema</TitleStyled>
-          <TextStyled bottom20>
-            Envíanos tus comentarios acerca de los problemas que encuentras en nuestra página. El equipo de fangel lo resolverá rápidamente.
-          </TextStyled>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <SubtitleStyled>Detalles</SubtitleStyled>
-            <TextAreaStyled border4 type="text" placeholder="Descripcion" name="reportDescription" ref={register({required:{value: true, message:"Campo requerido*"}})}/>
-            <ButtonsContainer>
-              <ButtonStyled onClick={recoverReportImage} secondary left type="button">Adjuntar una imagen</ButtonStyled>
-              <input type="file" accept="image/*" style={{display: "none"}} id="reportImage"/>
-              <ButtonStyled primary right type="submit">Enviar</ButtonStyled>
-            </ButtonsContainer>
-          </form>
-          <TextStyled bottom20>
-            Si necesitas ayuda de inmediato para solucionar un problema en concreto, ingresa al <Link to="/support">Centro de ayuda y asistencia</Link>.
-          </TextStyled>
-          <SubtitleStyled>Apoya con el proyecto</SubtitleStyled>
-          <TextStyled bottom20>
-            Envianos un email a <span>support@fangelweb.com</span> si tienes ideas para ayudar a mejorar fangel, o para ser parte del equipo de fangel.
-          </TextStyled>
-        </OnlyDesktop>
-      </Wrapper>
+      {
+        reportCreated?
+        <Redirect to={{
+          pathname: "/thank-you",
+        }}/>:
+        <Wrapper>
+          <OnlyDesktop>
+            <TitleStyled bottom>Reporta un problema</TitleStyled>
+            <TextStyled bottom20>
+              Envíanos tus comentarios acerca de los problemas que encuentras en nuestra página. El equipo de fangel lo resolverá rápidamente.
+            </TextStyled>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <SubtitleStyled>Detalles</SubtitleStyled>
+              <TextAreaStyled border4 type="text" placeholder="Descripcion" name="reportDescription" ref={register({required:{value: true, message:"Campo requerido*"}})}/>
+              <ButtonsContainer>
+                <ButtonStyled onClick={recoverReportImage} secondary left type="button">Adjuntar una imagen</ButtonStyled>
+                <input type="file" accept="image/*" style={{display: "none"}} id="reportImage"/>
+                <ButtonStyled primary right type="submit">Enviar</ButtonStyled>
+              </ButtonsContainer>
+            </form>
+            <TextStyled bottom20>
+              Si necesitas ayuda de inmediato para solucionar un problema en concreto, ingresa al <Link to="/support">Centro de ayuda y asistencia</Link>.
+            </TextStyled>
+            <SubtitleStyled>Apoya con el proyecto</SubtitleStyled>
+            <TextStyled bottom20>
+              Envianos un email a <span>support@fangelweb.com</span> si tienes ideas para ayudar a mejorar fangel, o para ser parte del equipo de fangel.
+            </TextStyled>
+          </OnlyDesktop>
+        </Wrapper>
+      }
       <ReturnPage/>
     </main>
   );
