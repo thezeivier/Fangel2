6c8bf63be147adcb980d2d7be4a3038919cf851b
drakembite <drakembite@gmail.com>
2021-05-28 12:04:46 -0500

    Update VideoAdmin

--- src/pages/inCommunity/VideoAdmin.js
+++ src/pages/inCommunity/VideoAdmin.js
@@ -4,6 +4,7 @@ import RegHeader from './../../components/general/RegHeader'
 import VideoHeader from './../../components/general/VideoHeader'
 import Footer from './../../components/general/Footer'
 import { ContainerForCommunity } from './../../components/general/InternalLayout'
+import MainSettingsAdmin from './../../components/settingsForAdmin/MainSettingsAdmin'
 
 import MainSpinner from '../../components/spinner/MainSpinner'
 
@@ -36,6 +37,12 @@ const VideoAdmin = ({communityData, isAdmin, communityDataSubSpace, isSubSpace})
             communityData={communityData}
             open={openMoldal} 
             isAdmin={isAdmin} 
+            isSubSpace={isSubSpace} 
+            communityDataSubSpace={communityDataSubSpace}/>
+          {/* Solo para desktop */}
+          <MainSettingsAdmin 
+            communityData={communityData}
+            inDesktop="grid"
             isSubSpace={isSubSpace} 
             communityDataSubSpace={communityDataSubSpace}/>
           <Footer noMobile />
