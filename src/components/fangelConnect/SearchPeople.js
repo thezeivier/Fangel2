dc957d6dbf11b3e52d9d7928700a65cc94ab1863
drakembite <drakembite@gmail.com>
2021-06-01 10:29:54 -0500

    SearchPeople estilos apra desktop v0.1 y svg agregado

--- src/components/fangelConnect/SearchPeople.js
+++ src/components/fangelConnect/SearchPeople.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Wrapper from './../general/Wrapper'
 import { PeopleContainer, TextBodyStyled, SearchPeopleContainer } from './styles/sSearchPeople'
+import { ReactComponent as ProfileSVG } from './../general/icons/profile.svg'
 
 const SearchPeople = ({ modalIsOpen }) => {
   return (
@@ -10,8 +11,10 @@ const SearchPeople = ({ modalIsOpen }) => {
           <TextBodyStyled>Buscando personas con tus mismos intereses</TextBodyStyled>
           <PeopleContainer>
             <img />
-            <p>Conectando</p>
-            <img />
+            {/* <ProfileSVG /> */}
+            <span>Conectando</span>
+            {/* <img /> */}
+            <ProfileSVG />
           </PeopleContainer>
           <a onClick={modalIsOpen}>Cancelar busqueda</a>
         </SearchPeopleContainer>
--- src/components/fangelConnect/styles/sSearchPeople.js
+++ src/components/fangelConnect/styles/sSearchPeople.js
@@ -31,6 +31,12 @@ export const SearchPeopleContainer = styled.div`
       text-decoration: underline;
     }
   }
+
+  @media(min-width:768px) {
+    a {
+      margin: 0 0 40px 0;
+    }
+  }
 `
 
 export const PeopleContainer = styled.div`
@@ -46,11 +52,36 @@ export const PeopleContainer = styled.div`
     background: gray;
   }
 
-  p {
+  svg {
+    fill: ${props => props.theme.textColor};
+    width: 100px;
+    height: 100px;
+  }
+
+  span {
     display: block;
     margin: 40px 0;
     color: ${props => props.theme.smallText};
     animation: ${Faded} 2s ease-out infinite;
+    cursor: default;
+  }
+
+  @media(min-width:1200px) {
+    flex-direction: row;
+
+    img {
+      width: 120px;
+      height: 120px;
+    }
+
+    svg {
+      width: 120px;
+      height: 120px;
+    }
+
+    span {
+      margin: 0 80px;
+    }
   }
 `
 
@@ -59,4 +90,8 @@ export const TextBodyStyled = styled(TextBody)`
   margin-top: 30px;
   width: 70%;
   font-weight: ${props => props.theme.weight.semiMedium};
+
+  @media(min-width:768px) {
+    margin-top: 40px;
+  }
 `
