0d06891bc75d0919dea208d4bfe971ac7e60d35c
thezeivier <zbeizaga@gmail.com>
2021-06-09 11:24:50 -0500

    Mejora de uso del minireproductor

--- src/components/general/VideoHeader.js
+++ src/components/general/VideoHeader.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {AppContext} from '../../App'
 import ButtonLeaveCom from './ButtonLeaveCom'
 import ModalSettingsAdmin from './../../pages/inCommunity/ModalSettingsAdmin'
 import { HeaderContainer, TitleCommunityStyled, ContainerSVG, OnlyUsersContainer } from './styles/sVideoHeader'
@@ -7,17 +8,18 @@ import { ReactComponent as VideoSettingsSVG } from './icons/videoSettings.svg'
 import { ReactComponent as CloseSVG } from './icons/close.svg'
 
 const VideoHeader = ({ displayNoAdmin, isSettings, modalIsOpen, communityData, closeModal, closeModalSA, onlyUsers, communityDataSubSpace, isSubSpace }) => {
+  const {setCommunityGlobalData} = useContext(AppContext)
   return (
     <>
       <HeaderContainer>
         <CommunitySVG />
         <TitleCommunityStyled as="h3">{!isSubSpace ? communityData.title : communityDataSubSpace.nameOfSpace}</TitleCommunityStyled>
         <OnlyUsersContainer onlyUsers={onlyUsers}>
-          <ButtonLeaveCom className="buttonOnlyMobile"/>
+          <ButtonLeaveCom setCommunityGlobalData={setCommunityGlobalData} className="buttonOnlyMobile"/>
         </OnlyUsersContainer>
         <ContainerSVG display={displayNoAdmin} isSettings={isSettings} communityData={communityData}>
           <div className="svgOnlyMobile">
-            <ButtonLeaveCom className="buttonOnlyMobile"/>
+            <ButtonLeaveCom setCommunityGlobalData={setCommunityGlobalData} className="buttonOnlyMobile"/>
           </div>
           <CloseSVG className="svgCloseOnlyMobile" onClick={closeModalSA} />
         </ContainerSVG>
