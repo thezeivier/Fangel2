04740fb0f5ba131bcb0bee627f360c02a6e92dff
thezeivier <zbeizaga@gmail.com>
2021-07-11 11:50:47 -0500

    Mejora de regex para email

--- src/pages/signInAndUp/objects/formValidators.js
+++ src/pages/signInAndUp/objects/formValidators.js
@@ -27,7 +27,7 @@ export const emailFValidator = {
     required: true,
     max: 45,
     pattern: {
-      value: /^[A-Z0-9._%+-]+@[A-Z0-9. -]+\.[A-Z ]{2,45}$/i,
+      value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i,
       message: "Correo electrónico inválido*",
     }
 }
