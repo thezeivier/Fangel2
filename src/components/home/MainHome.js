409252e589983c012a334eeb5fed166611acff2f
thezeivier <zbeizaga@gmail.com>
2021-08-25 19:01:54 -0500

    Pausa de FangelTV hasta validar

--- src/components/home/MainHome.js
+++ src/components/home/MainHome.js
@@ -67,7 +67,7 @@ const MainHome = () => {
             </CardsContainer>
         }
           <Suspense fallback={<p>Cargando...</p>}>
-          <CardFangelTv communityProvider={communityProvider}/>
+          {/* <CardFangelTv communityProvider={communityProvider}/> */}
           {
             communities &&
               communities.map((community)=>{ //Render list of "CardCommunity".
