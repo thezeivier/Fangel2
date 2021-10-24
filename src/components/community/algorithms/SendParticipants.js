60cf2bc1466adaff005368dc8b10e9ad4ba6ac01
kvn <khevinstools@gmail.com>
2021-10-24 12:25:38 -0500

    Fix Try SendParticipants

--- src/components/community/algorithms/SendParticipants.js
+++ src/components/community/algorithms/SendParticipants.js
@@ -2,18 +2,22 @@ import firebase from 'firebase/app'
 import { VerifyIsBusinessProfile } from './VerifyIsBusinessProfile'
 
 export const SendParticipants = async (firestore, userData, roomId, creatorUid) => {
-    const batch = firestore.batch()
-    const isBusinessAccount = await VerifyIsBusinessProfile(firestore, creatorUid)
-    if(isBusinessAccount.profileType != "business_beta") {
-        return null
-    } else {
-        batch.set(
-            firestore.collection("communities").doc(roomId),
-            {
-                allUsersDataConnected: firebase.firestore.FieldValue.arrayUnion(userData)
-            },
-            {merge: true}
-        )
-        return await batch.commit()
+    try {
+        const batch = firestore.batch()
+        const isBusinessAccount =  await VerifyIsBusinessProfile(firestore, creatorUid)
+        if(isBusinessAccount.profileType != "business_beta") {
+            return null
+        } else {
+            batch.set(
+                firestore.collection("communities").doc(roomId),
+                {
+                    allUsersDataConnected: firebase.firestore.FieldValue.arrayUnion(userData)
+                },
+                {merge: true}
+            )
+            return await batch.commit()
+        }
+    } catch(err) {
+        console.info("Bad Request")
     }
 }
